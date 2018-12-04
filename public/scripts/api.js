7c3fba69039039f660c3313df138daddd7abb25a
Ethan Frigon <ethan.frigon@gmail.com>
2018-12-04 12:28:55 -0800

    client changes

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,4 +1,4 @@
-/* global $ */
+/* global $ store */
 'use strict';
 
 const api = (function () {
@@ -7,25 +7,31 @@ const api = (function () {
       type: 'GET',
       url: path,
       dataType: 'json',
-      data: query
+      data: query,
+      headers: { 'Authorization': `Bearer: ${store.authToken}` }
     });
   };
+  
   const details = function (path) {
     return $.ajax({
       type: 'GET',
       dataType: 'json',
-      url: path
+      url: path,
+      headers: { 'Authorization': `Bearer: ${store.authToken}` }
     });
   };
+  
   const update = function (path, obj) {
     return $.ajax({
       type: 'PUT',
       url: path,
       contentType: 'application/json',
       dataType: 'json',
-      data: JSON.stringify(obj)
+      data: JSON.stringify(obj),
+      headers: { 'Authorization': `Bearer: ${store.authToken}` }
     });
   };
+  
   const create = function (path, obj) {
     return $.ajax({
       type: 'POST',
@@ -33,16 +39,20 @@ const api = (function () {
       contentType: 'application/json',
       dataType: 'json',
       processData: false,
-      data: JSON.stringify(obj)
+      data: JSON.stringify(obj),
+      headers: { 'Authorization': `Bearer: ${store.authToken}` }
     });
   };
+  
   const remove = function (path) {
     return $.ajax({
       type: 'DELETE',
       dataType: 'json',
-      url: path
+      url: path,
+      headers: { 'Authorization': `Bearer: ${store.authToken}` }
     });
   };
+  
   return {
     create,
     search,
