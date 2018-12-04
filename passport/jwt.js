74b483a010c1e2c9d870288eac6f22b1526024ac
Ethan Frigon <ethan.frigon@gmail.com>
2018-12-04 12:02:32 -0800

    created and exported jwt strategy

---
+++ passport/jwt.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const { JWT_SECRET } = require('../config');
+
+// const options = {
+//   secretOrKey: JWT_SECRET,
+//   jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+//   algorithms: ['HS256']
+// };
+
+// const jwtStrategy = new JwtStrategy(options, (payload, done) => {
+//   done(null, payload.user);
+// });
+
+// OR
+
+const jwtStrategy = new JwtStrategy(
+  {
+    secretOrKey: JWT_SECRET,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+    algorithms: ['HS256']
+  }, 
+  (payload, done) => {
+    done(null, payload.user);
+  });
+
+module.exports = jwtStrategy;
