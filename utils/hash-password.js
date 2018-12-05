8db86314f988080e6dd1942823c84004cf2f9b23
Ethan Frigon <ethan.frigon@gmail.com>
2018-12-05 08:14:09 -0800

    added hash-password util

---
+++ utils/hash-password.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+const password = 'baseball';
+
+bcrypt.hash(password, 10)
+  .then(digest => {
+    console.log('digest = ', digest);
+    return digest;
+  })
+  .catch(err => {
+    console.error('error', err);
+  });
